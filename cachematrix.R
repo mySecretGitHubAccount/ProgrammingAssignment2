9fb31144be358dff492f3867b14c78681e24e986
Pinhas <arielpinhas@gmail.com>
2020-04-23 11:08:57 +0300

    DeplyCacheInvMatrixFunc

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,25 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) x <<- y
+    get <- function() x
+    getInv <- function() inv
+    setInv <- function(inputInv) inv <<- inputInv
+    list(set = set, get = get, getInv = getInv, setInv=setInv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setInv(inv)
+    inv
 }
